a29c6699642564a0673d6c6bb983762d577707f1
coco2025 <coco2025@ejemplo.com>
2023-11-18 10:29:07 -0500

    segundo avance

---
+++ src/main/java/Controlador/ControladorAuto.java
@@ -0,0 +1,56 @@
+package Controlador;
+
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Dao.ClassCrudAuto;
+import model.TblAuto;
+
+/**
+ * Servlet implementation class ControladorAuto
+ */
+public class ControladorAuto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControladorAuto() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	       //intanciamos la respectiva clase...
+		ClassCrudAuto cruauto=new ClassCrudAuto();
+		TblAuto tblauto=new TblAuto();
+		//enviamos a la vista
+		List<TblAuto> listado=cruauto.Listado();
+		request.setAttribute("listado",listado);
+		//redireccionamos
+	request.getRequestDispatcher("/ListadoAutos.jsp").forward(request,response);
+		
+		
+		
+	}  //fin del metodo doget
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/ListadoAutos.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    
+<%@page import="java.util.*" %>   
+<%@page import="model.TblAuto" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Listado de autos</title>
+</head>
+<body bgcolor="#c5dec9">
+<h1 align="center">
+Listado de Autos
+</h1>
+<table align="center">
+<tr>
+<th>Codigo</th>
+<th>Color</th>
+<th>Modelo</th>
+<th>Placa</th>
+</tr>
+<%
+List<TblAuto> listar=(List<TblAuto>)request.getAttribute("listado");
+//aplicamos un bucle for.
+for(TblAuto lis:listar){
+	//aplicamos una condicion
+	if(lis!=null){
+		%>
+	<tr>
+	<td><%=lis.getIdauto() %></td>
+	<td><%=lis.getColor() %></td>
+	<td><%=lis.getModelo() %></td>
+	<td><%=lis.getMotor() %></td>
+	</tr>	
+		<%		
+	}//fin del if
+}  //fin del bucle for...
+%>
+</table>
+</body>
+</html>
